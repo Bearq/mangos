8ed8465f7c5280627af03a5aaafb80bc6f8a1452
michalpolko <michalpolko@o2.pl>
2011-09-16 23:28:25 +0200

    [a114] Some minor fixes for spells for Valithria Dreamwalker (ICC encounter).

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -2276,6 +2276,14 @@ bool SpellMgr::IsStackableSpellAuraHolder(SpellEntry const* spellInfo)
         }
     }
 
+    // some direct ID checks (hacks!)
+    switch(spellInfo->Id)
+    {
+        case 70602: // Corruption (Valithria Dreamwalker)
+        case 70588: // Suppression (Valithria Dreamwalker)
+            return true;
+    }
+
     return false;
 }
 
